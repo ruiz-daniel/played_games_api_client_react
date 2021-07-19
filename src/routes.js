f79f9de5f376af59fab012fb373a8678b685238c
ruiz-daniel <ruizd1710@gmail.com>
2021-07-19 12:24:43 -0400

    added more server routes

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+//client routes
 export const home = "/";
 export const playedgames = "/playedgames";
 export const tableview = "/tableview";
@@ -7,4 +8,9 @@ export const stats = "/stats";
 export const top10games = "/top10games";
 export const managetop10games = "/managetop10games";
 
+//server routes
 export const sr_top10games = "top10games";
+export const sr_playedgames = "PlayedGames/";
+export const sr_playinggames = "PlayedGames/status/3";
+export const sr_platforms = "PlayedGames/platforms/";
+export const sr_statuses = "PlayedGames/statuses/";
