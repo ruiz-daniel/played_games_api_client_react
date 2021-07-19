76b0102e03174d5ae995b22cba6b34a78cac9e3e
ruiz-daniel <ruizd1710@gmail.com>
2021-07-19 11:55:01 -0400

    feat: added progress bar

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import * as routes from "./routes";
 
 import "primereact/resources/primereact.min.css";
+import "nprogress/nprogress.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
