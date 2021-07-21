49369200524118f56bcb265146482a6dca31876c
ruiz-daniel <ruizd1710@gmail.com>
2021-07-21 14:00:34 -0400

    added scss files

---
+++ src/styles/styles.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const topbar = {
+  paddingTop: "0",
+  paddingBottom: "0",
+  paddingLeft: "3rem",
+  paddingRight: "3rem",
+  backgroundColor: "",
+};
+
+export const logo = {
+  marginRight: "2rem",
+};
+
+export const light_theme = {
+  backgroundColor: "white",
+};
+export const dark_theme = {
+  backgroundColor: "#241e2a",
+};
