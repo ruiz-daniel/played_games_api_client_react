dc691aaa2169f0a87c7285ee4a3c980a54ea12bf
ruiz-daniel <ruizd1710@gmail.com>
2021-07-21 21:12:00 -0400

    style, fix: fixed status colors and adjusted padding

--- src/components/status.js
+++ src/components/status.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const status = (props) => {
   const getStatus = () => {
-    if (props.status === "Completed") return "status score-good";
-    else if (props.status === "Dropped") return " status score-bad";
+    if (props.status === "Completed") return "status status-completed";
+    else if (props.status === "Dropped") return " status status-dropped";
     else if (
       props.status === "Playing" ||
       props.status === "Played" ||
       props.status === "On Hold"
     )
-      return "status score-mid";
+      return "status status-incomplete";
   };
   return <span className={getStatus()}>{props.status}</span>;
 };
