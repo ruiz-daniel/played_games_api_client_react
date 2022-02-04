c76e4db28bc9302c71f445ed2ed5f902778137b4
ruiz-daniel <ruizd1710@gmail.com>
2022-02-04 17:04:26 -0500

    feat: react hook form and validations to upload game page

--- src/components/pages/UploadGame.js
+++ src/components/pages/UploadGame.js
@@ -1,58 +1,46 @@
 import React, { useState, useRef } from "react";
 import api from "../../services/APICalls";
+import { useForm, Controller } from "react-hook-form";
 
 import { InputText } from "primereact/inputtext";
 import { Dropdown } from "primereact/dropdown";
 import { FileUpload } from "primereact/fileupload";
 import { Panel } from "primereact/panel";
 import { InputTextarea } from "primereact/inputtextarea";
+import { Button } from "primereact/button";
 
 import { Toast } from "primereact/toast";
 
 const UploadGame = () => {
-  const [name, setName] = useState("");
-  const [year, setYear] = useState("");
-  const [dev, setDev] = useState("");
-  const [publisher, setPublisher] = useState("");
-  const [genre, setGenre] = useState("");
-  const [rating, setRating] = useState("");
-  const [status, setStatus] = useState({ id: "", name: "" });
-  const [platform, setPlatform] = useState({ id: "", name: "" });
   const [platformList, setPlatformList] = useState([]);
   const [statusList, setStatusList] = useState([]);
-  const [descrption, setDescription] = useState("");
-  const [gameImage, setGameImage] = useState({ name: "none" });
+  const [gameImage, setGameImage] = useState();
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
 
   const toast = useRef(null);
 
-  const clear = () => {
-    setName("");
-    setYear("");
-    setDev("");
-    setPublisher("");
-    setGenre("");
-    setRating("");
-    setStatus("");
-    setPlatform("");
-    setDescription("");
-    setGameImage("");
-  };
-
-  const handleSubmit = () => {
+  const onSubmit = async (data) => {
+    await api.uploadImage(gameImage);
     api.postPlayedGame(
       {
-        name,
-        developer: dev,
-        publisher,
-        year,
-        genre,
-        rating,
-        platformid: platform.id,
-        statusid: status.id,
-        image: gameImage.name,
+        name: data.name,
+        developer: data.dev ? data.dev : "Unknown",
+        publisher: data.publisher ? data.publisher : "Unknown",
+        year: data.year ? data.year : "Unknown",
+        genre: data.genre ? data.genre : "Unknown",
+        rating: data.rating,
+        description: data.description,
+        platformid: data.platform.id,
+        statusid: data.status.id,
+        image: gameImage ? gameImage.name : "no-cover.jpg",
       },
       () => {
-        clear();
         toast.current.show({
           severity: "success",
           summary: "Game Uploaded Successfully",
@@ -64,151 +52,136 @@ const UploadGame = () => {
 
   React.useEffect(() => {
     (async () => {
-      const response = await api.fetchPlatforms();
-      const platforms = await response.data;
+      const response_platforms = await api.fetchPlatforms();
+      const platforms = await response_platforms.data;
       setPlatformList(platforms);
-    })();
-  }, []);
-
-  React.useEffect(() => {
-    (async () => {
-      const response = await api.fetchStatuses();
-      const statuses = await response.data;
+      const response_status = await api.fetchStatuses();
+      const statuses = await response_status.data;
       setStatusList(statuses);
     })();
   }, []);
 
   const onupload = async (e) => {
-    await api.uploadImage(e.files[0]);
     setGameImage(e.files[0]);
-    e.options.clear();
   };
 
   return (
     <Panel header="New Game" className="upload-game-form">
       <Toast ref={toast} />
-      <div className="upload-game-fields flex flex-column">
-        <div className="item">
-          <h5>Name *</h5>
-          <InputText
-            id="gname"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <span className="item">
-          <h5>Developer(s)</h5>
-          <InputText
-            id="gdev"
-            value={dev}
-            onChange={(e) => setDev(e.target.value)}
-          />
-        </span>
-        <span className="item">
-          <h5>Publisher(s)</h5>
-          <InputText
-            id="gpub"
-            value={publisher}
-            onChange={(e) => setPublisher(e.target.value)}
-          />
-        </span>
-        <span className="item">
-          <h5>Year</h5>
-          <InputText
-            id="gyear"
-            value={year}
-            onChange={(e) => setYear(e.target.value)}
-          />
-        </span>
-
-        <span className="item">
-          <h5>Genre(s)</h5>
-          <InputText
-            id="ggenre"
-            value={genre}
-            onChange={(e) => setGenre(e.target.value)}
-          />
-        </span>
-        <span className="item">
-          <h5>Description (Optional)</h5>
-          <InputTextarea
-            rows={5}
-            // cols={document.getElementById("ggenre").clientWidth / 10}
-            value={descrption}
-            onChange={(e) => setDescription(e.target.value)}
-            autoResize
-          />
-        </span>
-        <div className="items-platform-status-rating flex justify-content-between">
-          <span className="item item-platform">
-            <h5>Platform *</h5>
-            <Dropdown
-              id="gplatform"
-              value={platform}
-              options={platformList}
-              onChange={(e) => setPlatform(e.value)}
-              optionLabel="name"
-            />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="upload-game-fields flex flex-column">
+          <div className="item">
+            <h5>Name *</h5>
+            <InputText id="gname" {...register("name", { required: true })} />
+            {errors.name && (
+              <span className="error-message">Name is required</span>
+            )}
+          </div>
+          <span className="item">
+            <h5>Developer(s)</h5>
+            <InputText id="gdev" {...register("dev")} />
           </span>
-          <span className="item item-status">
-            <h5>Status *</h5>
-            <Dropdown
-              id="gstatus"
-              value={status}
-              options={statusList}
-              onChange={(e) => setStatus(e.value)}
-              optionLabel="name"
-            />
+          <span className="item">
+            <h5>Publisher(s)</h5>
+            <InputText id="gpub" {...register("publisher")} />
           </span>
-          <span className="item item-rating">
-            <h5>Rating *</h5>
+          <span className="item">
+            <h5>Year</h5>
             <InputText
-              id="grating"
-              value={rating}
+              id="gyear"
               type="number"
-              onChange={(e) => setRating(e.target.value)}
+              {...register("year", { min: 1970, max: 2030 })}
             />
+            {errors.year && (
+              <span className="error-message">Requires a valid year</span>
+            )}
           </span>
-        </div>
 
-        <div className="item">
-          <h5>Game Image</h5>
-          <FileUpload
-            name="gameImage"
-            customUpload
-            auto
-            uploadHandler={onupload}
-            accept="image/*"
-            chooseLabel="File"
-            emptyTemplate={
-              <p className="p-m-0">Drag and drop files to here to upload.</p>
-            }
-          />
+          <span className="item">
+            <h5>Genre(s)</h5>
+            <InputText id="ggenre" {...register("genre")} />
+          </span>
+          <span className="item">
+            <h5>Description (Optional)</h5>
+            <InputTextarea rows={5} {...register("description")} autoResize />
+          </span>
+          <div className="items-platform-status-rating flex justify-content-between">
+            <span className="item item-platform">
+              <h5>Platform *</h5>
+              <Controller
+                name="platform"
+                control={control}
+                rules={{ required: "Platform is required" }}
+                render={({ field, fieldState }) => (
+                  <Dropdown
+                    id={field.name}
+                    value={field.value}
+                    onChange={(e) => field.onChange(e.value)}
+                    options={platformList}
+                    optionLabel="name"
+                  />
+                )}
+              />
+              {errors.platform && (
+                <div className="error-message">Platform is required</div>
+              )}
+            </span>
+            <span className="item item-status">
+              <h5>Status *</h5>
+              <Controller
+                name="status"
+                control={control}
+                rules={{ required: "Status is required" }}
+                render={({ field, fieldState }) => (
+                  <Dropdown
+                    id={field.name}
+                    value={field.value}
+                    onChange={(e) => field.onChange(e.value)}
+                    options={statusList}
+                    optionLabel="name"
+                  />
+                )}
+              />
+              {errors.status && (
+                <div className="error-message">Status is required</div>
+              )}
+            </span>
+            <span className="item item-rating">
+              <h5>Rating *</h5>
+              <InputText
+                id="grating"
+                type="number"
+                {...register("rating", { min: 1, max: 10, required: true })}
+              />
+              {errors.rating && (
+                <div className="error-message">Requires a valid rating</div>
+              )}
+            </span>
+          </div>
+
+          <div className="item">
+            <h5>Image</h5>
+            <FileUpload
+              name="gameImage"
+              customUpload
+              auto
+              uploadHandler={onupload}
+              accept="image/*"
+              chooseLabel="File"
+              emptyTemplate={
+                <p className="p-m-0">Drag and drop files to here to upload.</p>
+              }
+            />
+          </div>
+          <div className="item mt-4">
+            <Button
+              className="upload-button"
+              label="Upload Game"
+              type="submit"
+            ></Button>
+          </div>
         </div>
-        <div className="item">
-          <h2 className="upload-button" onClick={handleSubmit}>
-            <div>
-              <i className="pi pi-check"></i> Upload Game
-            </div>
-          </h2>
-        </div>
-      </div>
-      {/* <div className="p-col-4 upload-game-preview">
-        <h2>Preview</h2>
-        <GameBox
-          game={{
-            name,
-            developer: dev,
-            publisher,
-            year,
-            rating,
-            genre,
-            status,
-            platform,
-            image: sr_images + gameImage.name,
-          }}
-        ></GameBox>
-      </div> */}
+      </form>
     </Panel>
   );
 };
