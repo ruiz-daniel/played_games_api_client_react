25881db188ae09798ae8db8edb507e5a365d5b14
ruiz-daniel <ruizd1710@gmail.com>
2022-02-05 17:21:20 -0500

    fix, performance: Stats page
    fixed page layout
    fixed page loading

--- src/components/pages/GameStats.js
+++ src/components/pages/GameStats.js
@@ -22,6 +22,62 @@ const GameStats = () => {
     datasets: [{}],
   });
 
+  //VARIABLES FOR DATA
+  var completed = 0;
+  var dropped = 0;
+  var played = 0;
+  var onHold = 0;
+
+  var pc = 0;
+  var ps3 = 0;
+  var ps2 = 0;
+  var ps1 = 0;
+  var gba = 0;
+  var nds = 0;
+  var ds3 = 0;
+  var nswitch = 0;
+  var wiiu = 0;
+  var psp = 0;
+  var wii = 0;
+
+  var y1996 = 0;
+  var y1997 = 0;
+  var y1998 = 0;
+  var y1999 = 0;
+  var y2000 = 0;
+  var y2001 = 0;
+  var y2002 = 0;
+  var y2003 = 0;
+  var y2004 = 0;
+  var y2005 = 0;
+  var y2006 = 0;
+  var y2007 = 0;
+  var y2008 = 0;
+  var y2009 = 0;
+  var y2010 = 0;
+  var y2011 = 0;
+  var y2012 = 0;
+  var y2013 = 0;
+  var y2014 = 0;
+  var y2015 = 0;
+  var y2016 = 0;
+  var y2017 = 0;
+  var y2018 = 0;
+  var y2019 = 0;
+  var y2020 = 0;
+  var y2021 = 0;
+  var y2022 = 0;
+
+  var sc2 = 0;
+  var sc3 = 0;
+  var sc4 = 0;
+  var sc5 = 0;
+  var sc6 = 0;
+  var sc7 = 0;
+  var sc8 = 0;
+  var sc9 = 0;
+  var sc10 = 0;
+
   const completionOptions = {
     plugins: {
       legend: {
@@ -122,44 +178,20 @@ const GameStats = () => {
     },
   };
 
-  const getCompletionStats = (data) => {
-    let completed = 0;
-    let dropped = 0;
-    let played = 0;
+  const getStats = (data) => {
     data.forEach((element) => {
+      //COMPLETION
       if (element.status.name === "Completed") {
         completed++;
       } else if (element.status.name === "Dropped") {
         dropped++;
       } else if (element.status.name === "Played") {
         played++;
+      } else if (element.status.name === "On Hold") {
+        onHold++;
       }
-    });
-    setcompletionChart({
-      labels: ["Completed", "Played", "Dropped"],
-      datasets: [
-        {
-          data: [completed, played, dropped],
-          backgroundColor: ["#12c941", "#ffff00", "#d71c2f"],
-          hoverBackgroundColor: ["#12b13a", "#d4d401", "#c51d2e"],
-        },
-      ],
-    });
-  };
 
-  const getPlatformStats = (data) => {
-    let pc = 0;
-    let ps3 = 0;
-    let ps2 = 0;
-    let ps1 = 0;
-    let gba = 0;
-    let nds = 0;
-    let ds3 = 0;
-    let nswitch = 0;
-    let wiiu = 0;
-    let psp = 0;
-    let wii = 0;
-    data.forEach((element) => {
+      //PLATFORMS
       if (element.platform.name === "PC") {
         pc++;
       } else if (element.platform.name === "Nintendo DS") {
@@ -183,60 +215,8 @@ const GameStats = () => {
       } else if (element.platform.name === "PSP") {
         psp++;
       }
-      setPlatformChart({
-        labels: [
-          "PC",
-          "PS1",
-          "PS2",
-          "PS3",
-          "PSP",
-          "GBA",
-          "Wii",
-          "WiiU",
-          "NDS",
-          "3DS",
-          "Switch",
-        ],
-        datasets: [
-          {
-            label: "Games per Platform",
-            backgroundColor: "#42A5F5",
-            data: [pc, ps1, ps2, ps3, psp, gba, wii, wiiu, nds, ds3, nswitch],
-          },
-        ],
-      });
-    });
-  };
 
-  const getYearsStats = (data) => {
-    let y1996 = 0;
-    let y1997 = 0;
-    let y1998 = 0;
-    let y1999 = 0;
-    let y2000 = 0;
-    let y2001 = 0;
-    let y2002 = 0;
-    let y2003 = 0;
-    let y2004 = 0;
-    let y2005 = 0;
-    let y2006 = 0;
-    let y2007 = 0;
-    let y2008 = 0;
-    let y2009 = 0;
-    let y2010 = 0;
-    let y2011 = 0;
-    let y2012 = 0;
-    let y2013 = 0;
-    let y2014 = 0;
-    let y2015 = 0;
-    let y2016 = 0;
-    let y2017 = 0;
-    let y2018 = 0;
-    let y2019 = 0;
-    let y2020 = 0;
-    let y2021 = 0;
-
-    data.forEach((element) => {
+      //YEARS
       if (element.year === "1996") y1996++;
       else if (element.year === "1997") y1997++;
       else if (element.year === "1998") y1998++;
@@ -263,7 +243,60 @@ const GameStats = () => {
       else if (element.year === "2019") y2019++;
       else if (element.year === "2020") y2020++;
       else if (element.year === "2021") y2021++;
+      else if (element.year === "2022") y2022++;
+
+      //SCORES
+      if (element.rating == 2) sc2++;
+      else if (element.rating == 3) sc3++;
+      else if (element.rating == 4) sc4++;
+      else if (element.rating == 5) sc5++;
+      else if (element.rating == 6) sc6++;
+      else if (element.rating == 7) sc7++;
+      else if (element.rating == 8) sc8++;
+      else if (element.rating == 9) sc9++;
+      else if (element.rating == 10) sc10++;
     });
+  };
+
+  const getCompletionStats = () => {
+    setcompletionChart({
+      labels: ["Completed", "Played", "Dropped", "On Hold"],
+      datasets: [
+        {
+          data: [completed, played, dropped, onHold],
+          backgroundColor: ["#12c941", "#ffff00", "#d71c2f", "#fea604"],
+          hoverBackgroundColor: ["#12b13a", "#d4d401", "#c51d2e", "#e39506"],
+        },
+      ],
+    });
+  };
+
+  const getPlatformStats = () => {
+    setPlatformChart({
+      labels: [
+        "PC",
+        "PS1",
+        "PS2",
+        "PS3",
+        "PSP",
+        "GBA",
+        "Wii",
+        "WiiU",
+        "NDS",
+        "3DS",
+        "Switch",
+      ],
+      datasets: [
+        {
+          label: "Games per Platform",
+          backgroundColor: "#42A5F5",
+          data: [pc, ps1, ps2, ps3, psp, gba, wii, wiiu, nds, ds3, nswitch],
+        },
+      ],
+    });
+  };
+
+  const getYearsStats = () => {
     setYearsChart({
       labels: [
         "1996",
@@ -292,44 +325,9 @@ const GameStats = () => {
         "2019",
         "2020",
         "2021",
+        "2022",
       ],
       datasets: [
-        {
-          type: "line",
-          label: "Games per Year",
-          backgroundColor: "#e00dbb",
-          borderWidth: 2,
-          fill: false,
-          tension: 0.4,
-          data: [
-            y1996,
-            y1997,
-            y1998,
-            y1999,
-            y2000,
-            y2001,
-            y2002,
-            y2003,
-            y2004,
-            y2005,
-            y2006,
-            y2007,
-            y2008,
-            y2009,
-            y2010,
-            y2011,
-            y2012,
-            y2013,
-            y2014,
-            y2015,
-            y2016,
-            y2017,
-            y2018,
-            y2019,
-            y2020,
-            y2021,
-          ],
-        },
         {
           type: "bar",
           label: "Games per Year",
@@ -361,33 +359,14 @@ const GameStats = () => {
             y2019,
             y2020,
             y2021,
+            y2022,
           ],
         },
       ],
     });
   };
 
-  const getScoreStats = (data) => {
-    let sc2 = 0;
-    let sc3 = 0;
-    let sc4 = 0;
-    let sc5 = 0;
-    let sc6 = 0;
-    let sc7 = 0;
-    let sc8 = 0;
-    let sc9 = 0;
-    let sc10 = 0;
-    data.forEach((element) => {
-      if (element.rating == 2) sc2++;
-      else if (element.rating == 3) sc3++;
-      else if (element.rating == 4) sc4++;
-      else if (element.rating == 5) sc5++;
-      else if (element.rating == 6) sc6++;
-      else if (element.rating == 7) sc7++;
-      else if (element.rating == 8) sc8++;
-      else if (element.rating == 9) sc9++;
-      else if (element.rating == 10) sc10++;
-    });
+  const getScoreStats = () => {
     setScoresChart({
       labels: ["2", "3", "4", "5", "6", "7", "8", "9", "10"],
       datasets: [
@@ -403,39 +382,34 @@ const GameStats = () => {
   useEffect(() => {
     api.getPlayedGames((data) => {
       setGames(data);
-      getCompletionStats(data);
-      getPlatformStats(data);
-      getYearsStats(data);
-      getScoreStats(data);
+      getStats(data);
+      getCompletionStats();
+      getPlatformStats();
+      getYearsStats();
+      getScoreStats();
     });
   }, []);
 
   return (
     <div className="stats-wrapper">
       <h1>Played Games Stats</h1>
-      <div className="p-grid">
-        <div className="p-col-4">
-          <h2>Completion Rate</h2>
-          <Chart
-            type="pie"
-            data={completionChart}
-            options={completionOptions}
-            style={{ position: "relative" }}
-          />
-        </div>
-        <div className="p-col-8">
-          <Chart type="bar" data={platformChart} options={platformOptions} />
-        </div>
+      <div className="stats-item">
+        <h2>Completion Rate</h2>
+        <Chart
+          type="pie"
+          data={completionChart}
+          options={completionOptions}
+          style={{ position: "relative", width: "20%" }}
+        />
       </div>
-      <div className="p-grid">
-        <div className="p-col-12">
-          <Chart type="bar" data={yearsChart} options={yearsOptions} />
-        </div>
+      <div className="stats-item">
+        <Chart type="bar" data={platformChart} options={platformOptions} />
       </div>
-      <div className="p-grid">
-        <div className="p-col-12">
-          <Chart type="bar" data={scoresChart} options={scoreOptions} />
-        </div>
+      <div className="stats-item">
+        <Chart type="bar" data={yearsChart} options={yearsOptions} />
+      </div>
+      <div className="stats-item">
+        <Chart type="bar" data={scoresChart} options={scoreOptions} />
       </div>
     </div>
   );
