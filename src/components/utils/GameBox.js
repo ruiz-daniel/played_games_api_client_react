b7fdf310675d1fb65cb751421ac47a01f44b358b
DRGWeiss <ruizd1710@gmail.com>
2023-01-27 07:47:47 -0500

    default no game cover image

--- src/components/utils/GameBox.js
+++ src/components/utils/GameBox.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import * as routes from '../../routes'
+import  no_cover  from '../../images/no-cover.jpg'
 
 import Score from './score'
 import Status from './status'
@@ -20,20 +21,15 @@ const GameBox = (props) => {
   return (
     <div className="game-box-container">
       <div className="game-box-img" onClick={editEvent}>
-        <img
-          alt="Game Cover"
-          src={
-            props.game.cover
-              ? props.game.cover
-              : 'https://localhost:5001/game_images/no-cover.jpg'
-          }
-        />
+        <img alt="Game Cover" src={props.game?.cover || no_cover} />
         <div className="game-details-panel">
           <div className="details-icon">
             <span className="pi pi-pencil"></span>
           </div>
         </div>
-        {props.game.completion && <Status status={props.game.completion.name}></Status>}
+        {props.game.completion && (
+          <Status status={props.game.completion.name}></Status>
+        )}
         {props.game.score && <Score score={props.game.score}></Score>}
       </div>
 
