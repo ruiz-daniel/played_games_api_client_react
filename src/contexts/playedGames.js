6b6a6efcb1a0eccfe718f7e8b56682d168807302
DRGWeiss <ruizd1710@gmail.com>
2023-04-26 18:09:06 -0400

    wip: played games context

---
+++ src/contexts/playedGames.js
@@ -0,0 +1,67 @@
+import { useState, createContext } from 'react'
+import api from '../services/IApi'
+
+export const PlayedGamesContext = createContext()
+
+let gamesBackup = []
+// Keep the filtered games when component unmounts
+let filteringGames = []
+
+export function PlayedGamesProvider({ children }) {
+  const [games, setGames] = useState([])
+
+  function getGames(callback) {
+    api.PlayedGamesApi.getPlayedGames(
+      localStorage.getItem('userid'),
+      (data) => {
+        if (filteringGames.length) {
+          setGames(filteringGames)
+        } else {
+          setGames(data)
+        }
+        gamesBackup = data
+      },
+      (error) => {
+        console.log(
+          '🚀 ~ file: PlayedGamesList.js ~ line 56 ~ useEffect ~ error',
+          error,
+        )
+      }
+    )
+  }
+
+  const localFilter = (value) => {
+    const filtered = []
+    gamesBackup.forEach((game) => {
+      let found = false
+      Object.keys(game).forEach((key) => {
+        if (!['played_hours', 'id', '_id'].includes(key)) {
+          if (['completion', 'platform'].includes(key)) {
+            if (game[key].name == value) {
+              found = true
+            }
+          } else if (typeof game[key] === 'string') {
+            if (game[key].includes(value)) {
+              found = true
+            }
+          } else if (game[key].length) {
+            // Array
+            if (game[key].toString().includes(value)) {
+              console.log(key)
+              found = true
+            }
+          } else {
+            if (game[key] == value) {
+              found = true
+            }
+          }
+        }
+      })
+      if (found) {
+        filtered.push(game)
+      }
+    })
+    filteringGames = filtered
+    setGames(filtered)
+  }
+}
