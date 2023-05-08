21ab408e04624299766288bc5d827d60fec95819
DRGWeiss <ruizd1710@gmail.com>
2023-05-08 16:01:50 -0400

    upload game form component

---
+++ src/components/utils/forms/UploadGameForm.js
@@ -0,0 +1,272 @@
+import { useState } from 'react'
+import { usePlatforms } from '../../../hooks/usePlatforms'
+import { useCompletions } from '../../../hooks/useCompletions'
+import { useForm, Controller } from 'react-hook-form'
+import { useMessages } from '../../../hooks/useMessages'
+
+import { InputText } from 'primereact/inputtext'
+import { Dropdown } from 'primereact/dropdown'
+import { Button } from 'primereact/button'
+import { InputTextarea } from 'primereact/inputtextarea'
+import { Chips } from 'primereact/chips'
+import { Dialog } from 'primereact/dialog'
+import GameImages from './GameImages'
+
+const UploadGameForm = ({ onSubmit }) => {
+  const { platforms } = usePlatforms()
+  const { completions } = useCompletions()
+  const { message } = useMessages()
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({})
+
+  const [showUploader, setShowUploader] = useState()
+  const [images, setImages] = useState()
+
+  const prepareSubmit = (data) => {
+    if (images) {
+      data.images = images
+    }
+    onSubmit(data)
+  }
+
+  const handleImagesSubmit = (images) => {
+    setImages(images)
+    setShowUploader(false)
+    message('info', 'Saved Images')
+  }
+
+  return (
+    <div style={{ width: '100%' }}>
+      <Dialog header="Game Images" visible={showUploader} style={{ width: '50vw' }} onHide={() => setShowUploader(false)}>
+        <GameImages onSubmit={handleImagesSubmit} />
+      </Dialog>
+      <form onSubmit={handleSubmit(prepareSubmit)}>
+        <div
+          className="flex flex-column edit-game-form"
+          style={{ width: '100%' }}
+        >
+          <div className="item flex flex-column">
+            <label htmlFor="gname">Name*</label>
+            <InputText
+              id="gname"
+              {...register('name', { required: true })}
+            />
+            {errors.name && (
+              <div className="error-message">Name is required</div>
+            )}
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="gdev">Developers</label>
+            <Controller
+              name="developers"
+              control={control}
+              render={({ field, fieldState }) => (
+                <Chips
+                  id="gdev"
+                  allowDuplicate={false}
+                  separator=","
+                  value={field.value}
+                  onChange={(e) => field.onChange(e.value)}
+                />
+              )}
+            />
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="gpub">Publisher</label>
+            <Controller
+              name="publishers"
+              control={control}
+              render={({ field, fieldState }) => (
+                <Chips
+                  id="gpub"
+                  allowDuplicate={false}
+                  separator=","
+                  value={field.value}
+                  onChange={(e) => field.onChange(e.value)}
+                />
+              )}
+            />
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="gyear">Release Year</label>
+            <InputText
+              id="gyear"
+              type="number"
+              onWheel={(e) => {e.target.blur()}}
+              {...register('release_year', { min: 1970, max: 2030 })}
+            />
+            {errors.release_year && (
+              <div className="error-message">Requires a valid year</div>
+            )}
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="gplayedyear">Played Year</label>
+            <InputText
+              id="gplayedyear"
+              type="number"
+              onWheel={(e) => {e.target.blur()}}
+              {...register('played_year', { min: 1970, max: 2030 })}
+            />
+            {errors.played_year && (
+              <div className="error-message">Requires a valid year</div>
+            )}
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="ggenre">Genres</label>
+            <Controller
+              name="genres"
+              control={control}
+              render={({ field, fieldState }) => (
+                <Chips
+                  id="gdev"
+                  allowDuplicate={false}
+                  separator=","
+                  value={field.value}
+                  onChange={(e) => field.onChange(e.value)}
+                />
+              )}
+            />
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="gtags">Tags</label>
+            <Controller
+              name="tags"
+              control={control}
+              render={({ field, fieldState }) => (
+                <Chips
+                  id="gtags"
+                  allowDuplicate={false}
+                  separator=","
+                  value={field.value}
+                  onChange={(e) => field.onChange(e.value)}
+                />
+              )}
+            />
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="platform">Platform</label>
+            <Controller
+              name="platform"
+              control={control}
+              render={({ field, fieldState }) => (
+                <Dropdown
+                  id={field.name}
+                  value={field.value}
+                  onChange={(e) => field.onChange(e.value)}
+                  options={platforms}
+                  optionLabel="name"
+                />
+              )}
+            />
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="status">Completion</label>
+            <Controller
+              name="completion"
+              control={control}
+              render={({ field, fieldState }) => (
+                <Dropdown
+                  id={field.name}
+                  value={field.value}
+                  onChange={(e) => field.onChange(e.value)}
+                  options={completions}
+                  optionLabel="name"
+                />
+              )}
+            />
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="grating">Score</label>
+            <InputText
+              id="grating"
+              type="number"
+              onWheel={(e) => {e.target.blur()}}
+              min={1}
+              max={10}
+              {...register('score', { min: 1, max: 10 })}
+            />
+            {errors.rating && (
+              <div className="error-message">Requires a valid score</div>
+            )}
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="ghours">Played Hours</label>
+            <InputText
+              id="ghours"
+              type="number"
+              min={0}
+              onWheel={(e) => {e.target.blur()}}
+              {...register('played_hours')}
+            />
+            {errors.played_hours && (
+              <div className="error-message">
+                Requires a valid amount of hours
+              </div>
+            )}
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="gsteam">Steam Page URL</label>
+            <InputText
+              id="gsteam"
+              {...register('steam_page')}
+            />
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="gepic">Epic Store Page URL</label>
+            <InputText
+              id="gepic"
+              {...register('epic_page')}
+            />
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="gother">Other Stores URLs</label>
+            <Controller
+              name="other_stores"
+              control={control}
+              render={({ field, fieldState }) => (
+                <Chips
+                  id="gother"
+                  allowDuplicate={false}
+                  separator=","
+                  value={field.value}
+                  onChange={(e) => field.onChange(e.value)}
+                />
+              )}
+            />
+          </div>
+          <div className="item flex flex-column">
+            <label htmlFor="gdesc">Description (optional)</label>
+            <InputTextarea
+              id="gdesc"
+              rows={5}
+              {...register('description')}
+              autoResize
+            />
+          </div>
+
+          <div className="flex justify-content-end mt-2 gap-4">
+            <Button
+              label="Images"
+              type='button'
+              icon="pi pi-camera"
+              className="pink-button"
+              onClick={() => setShowUploader(true)}
+            />
+            <Button
+              label="Upload"
+              type="submit"
+              icon="pi pi-upload"
+              className="p-button-outlined edit-button"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default UploadGameForm
