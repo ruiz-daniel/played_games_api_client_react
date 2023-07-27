ac4e26772f5fc3fab1a227796a67c7b748731815
DRGWeiss <ruizd1710@gmail.com>
2023-07-27 16:22:08 -0400

    wip: filtering from the backend

--- src/hooks/usePlayedGames.js
+++ src/hooks/usePlayedGames.js
@@ -44,6 +44,7 @@ export function usePlayedGames() {
       localStorage.getItem('userid'),
       page,
       null,
+      null,
       (response) => {
         setGamesAction(response.data.games)
         setPage(response.data.page)
@@ -97,62 +98,98 @@ export function usePlayedGames() {
   }
 
 
-  const externalFilter = (data) => {
-    let filtered = []
+  // const externalFilter = (data) => {
+  //   let filtered = []
 
-    gamesOnStore.forEach((game) => {
-      // Compare filters using include. Empty filters will not have effect
-      if (
-        compareIgnoreCase(game.name, data.name) &&
-        compareIgnoreCase(game.developers.toString(), data.developer) &&
-        compareIgnoreCase(game.publishers.toString(), data.publisher) &&
-        compare(data.year, game.release_year) &&
-        compare(data.played_year, game.played_year) &&
-        compare(data.played_hoursMin, game.played_hours, 'le') &&
-        compare(data.played_hoursMax, game.played_hours, 'ge') &&
-        compareIgnoreCase(game.genres.toString(), data.genre) &&
-        compare(data.score, game.score) &&
-        compareIgnoreCase(
-          game.platform.name,
-          data.platform
-            ? typeof data.platform === 'string'
-              ? data.platform
-              : data.platform.name
-            : '',
-        ) &&
-        compareIgnoreCase(
-          game.completion.name,
-          data.completion ? (typeof data.completion === 'string' ? data.completion : data.completion.name) : '',
-        )
-      ) {
-        filtered.push(game)
-        // Immediately pop out if it matches a filter-out condition
-        if (
-          (data.nameOut !== '' && compareIgnoreCase(game.name, data.nameOut)) ||
-          (data.developerOut !== '' && compareIgnoreCase(game.developers.toString(), data.developerOut)) ||
-          (data.publisherOut !== '' &&
-            compareIgnoreCase(game.publishers.toString(), data.publisherOut)) ||
-          (data.yearOut !== '' && (game.release_year?.includes(data.yearOut) || !game.release_year)) ||
-          (data.played_yearOut !== '' &&
-            (game.played_year?.includes(data.played_yearOut) ||
-              !game.played_year)) ||
-          (data.genreOut !== '' && compareIgnoreCase(game.genres.toString(), data.genreOut)) ||
-          (data.scoreOut !== '' && game.score == data.scoreOut) ||
-          (data.platformOut !== undefined &&
-            compareIgnoreCase(
-              game.platform.name,
-              typeof platformOut === 'string' ? data.platformOut : data.platformOut.name,
-            )) ||
-          (data.completionOut !== undefined &&
-            compareIgnoreCase(
-              game.completion.name,
-              typeof completionOut === 'string' ? data.completionOut : data.completionOut.name,
-            ))
-        )
-          filtered.pop()
+  //   gamesOnStore.forEach((game) => {
+  //     // Compare filters using include. Empty filters will not have effect
+  //     if (
+  //       compareIgnoreCase(game.name, data.name) &&
+  //       compareIgnoreCase(game.developers.toString(), data.developer) &&
+  //       compareIgnoreCase(game.publishers.toString(), data.publisher) &&
+  //       compare(data.year, game.release_year) &&
+  //       compare(data.played_year, game.played_year) &&
+  //       compare(data.played_hoursMin, game.played_hours, 'le') &&
+  //       compare(data.played_hoursMax, game.played_hours, 'ge') &&
+  //       compareIgnoreCase(game.genres.toString(), data.genre) &&
+  //       compare(data.score, game.score) &&
+  //       compareIgnoreCase(
+  //         game.platform.name,
+  //         data.platform
+  //           ? typeof data.platform === 'string'
+  //             ? data.platform
+  //             : data.platform.name
+  //           : '',
+  //       ) &&
+  //       compareIgnoreCase(
+  //         game.completion.name,
+  //         data.completion ? (typeof data.completion === 'string' ? data.completion : data.completion.name) : '',
+  //       )
+  //     ) {
+  //       filtered.push(game)
+  //       // Immediately pop out if it matches a filter-out condition
+  //       if (
+  //         (data.nameOut !== '' && compareIgnoreCase(game.name, data.nameOut)) ||
+  //         (data.developerOut !== '' && compareIgnoreCase(game.developers.toString(), data.developerOut)) ||
+  //         (data.publisherOut !== '' &&
+  //           compareIgnoreCase(game.publishers.toString(), data.publisherOut)) ||
+  //         (data.yearOut !== '' && (game.release_year?.includes(data.yearOut) || !game.release_year)) ||
+  //         (data.played_yearOut !== '' &&
+  //           (game.played_year?.includes(data.played_yearOut) ||
+  //             !game.played_year)) ||
+  //         (data.genreOut !== '' && compareIgnoreCase(game.genres.toString(), data.genreOut)) ||
+  //         (data.scoreOut !== '' && game.score == data.scoreOut) ||
+  //         (data.platformOut !== undefined &&
+  //           compareIgnoreCase(
+  //             game.platform.name,
+  //             typeof platformOut === 'string' ? data.platformOut : data.platformOut.name,
+  //           )) ||
+  //         (data.completionOut !== undefined &&
+  //           compareIgnoreCase(
+  //             game.completion.name,
+  //             typeof completionOut === 'string' ? data.completionOut : data.completionOut.name,
+  //           ))
+  //       )
+  //         filtered.pop()
+  //     }
+  //   })
+  //   setGames(filtered)
+  // }
+
+  const externalFilter = (data) => {
+    const filterData = {
+      name: data.name,
+      developers: data.developer,
+      publishers: data.publisher,
+      release_year: data.release_year,
+      played_year: data.played_year,
+      genre: data.genre,
+      score: data.score,
+      tags: data.tags,
+      played_hours: {$gte: data.played_hours_min}
+    }
+    api.PlayedGamesApi.getPlayedGames(
+      localStorage.getItem('userid'),
+      page,
+      100000,
+      filterData,
+      (response) => {
+        setGamesAction(response.data.games)
+        setPage(response.data.page)
+        setMax(response.data.max)
+      },
+      (error) => {
+        if (error.response.status === 403 || error.response.status === 401) {
+          message('warn', 'Session Expired. Please Login')
+        } else {
+          console.log(
+          '🚀 ~ file: PlayedGamesList.js ~ line 56 ~ useEffect ~ error',
+          error,
+          )
+          message('error', error.message)
+        }
       }
-    })
-    setGames(filtered)
+    )
   }
 
   const compareIgnoreCase = (stringA, stringB) => {
