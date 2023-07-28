63374352ef2524ad399a21c0808ff78978acdfaa
DRGWeiss <ruizd1710@gmail.com>
2023-07-28 16:01:33 -0400

    remove old api folder

--- src/services/PlayedGamesApi.js
+++ src/services/PlayedGamesApi.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { apiClient } from './GeneralApi'
-import oldApi from '../services old/IApi'
 
 export default {
   getPlayedGames(userid, page = 1, limit = 50, filterData, callback, errorFunction) {
@@ -105,53 +104,5 @@ export default {
       .catch((error) => {
         console.log(error)
       })
-  },
-  getUserGamesInfo(userid, callback, errorFunction) {
-    apiClient
-      .request({
-        method: 'get',
-        url: `PlayedGames/gamesinfo/${userid}`,
-      })
-      .then((response) => {
-        callback(response)
-      })
-      .catch((error) => {
-        errorFunction(error)
-      })
-  },
-  loginVersion1(oldCredentials, callback) {
-    oldApi.UserApi.login(
-      oldCredentials,
-      (data) => {
-        sessionStorage.setItem('access_token_v1', data.access_token)
-        sessionStorage.setItem('userid_v1', data.userid)
-        window.location.reload()
-      },
-      (error) => {
-        console.log(error.message)
-      },
-    )
-  },
-  importFromVersion1(userid) {
-    oldApi.PlayedGamesApi.getPlayedGames(
-      sessionStorage.getItem('userid_v1'),
-      (data) => {
-        data.forEach((game) => {
-          this.postPlayedGame(
-            {
-              name: game.name,
-              developers: game.developer.split(', '),
-              publishers: game.publisher.split(', '),
-              release_year: game.year,
-              score: game.rating,
-              description: game.description,
-              steam_page: game.steam_page || undefined,
-              played_hours: game.played_hours || undefined,
-            },
-            () => {},
-          )
-        })
-      },
-    )
   },
 }
