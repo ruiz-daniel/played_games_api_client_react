07fe590946f480a86aca5a53682f01793ad7eddd
DRGWeiss <ruizd1710@gmail.com>
2023-08-01 21:44:56 -0400

    game image viewer component
    - component for header, image and images viewing

--- src/components/pages/GameDetails.js
+++ src/components/pages/GameDetails.js
@@ -3,15 +3,12 @@ import { useToggle } from '../../hooks/useToggle'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { playedgames } from '../../routes'
 
-import { Image } from 'primereact/image'
-import { Button } from 'primereact/button'
 import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog'
 import { Dialog } from 'primereact/dialog'
 
-import GameImages from '../utils/forms/GameImages'
+import GameImageViewer from '../utils/cards/GameImageViewer'
 import EditGame from '../utils/forms/EditGame'
 import GameInfo from '../utils/cards/GameInfo'
-import  no_cover  from '../../images/no-cover.jpg'
 
 const GameDetails = () => {
   const navigator = useNavigate()
@@ -60,34 +57,7 @@ const GameDetails = () => {
         >
           <EditGame game={game} onSubmit={onUpdate} />
         </Dialog>
-        <Dialog 
-          header="Game Images" 
-          visible={images.toggleValue} 
-          style={{ width: '50vw' }} 
-          onHide={images.toggle}
-        >
-          <GameImages onSubmit={handleImagesSubmit} />
-        </Dialog>
-
-        <h2>{game.name}</h2>
-        <div className="game-details-image flex flex-column ">
-          <Image src={game.cover || no_cover} alt={game.name} preview />
-
-          <div className="game-details-buttons flex justify-content-end gap-3 mt-2">
-            <Button className='pink-button' label='Channge Images' onClick={images.toggle} />
-            <Button
-              icon='pi pi-pencil'
-              className="p-button-outlined p-button-rounded p-button-warning edit-button"
-              onClick={editing.toggle}
-            />
-            <Button
-              icon="pi pi-trash"
-              className="p-button-rounded p-button-outlined p-button-danger delete-button"
-              onClick={confirm}
-            />
-            <ConfirmDialog />
-          </div>
-        </div>
+        <GameImageViewer game={game} handleImagesSubmit={handleImagesSubmit}  />
         <GameInfo game={game} />
 
         
---
+++ src/components/utils/cards/GameImageViewer.js
@@ -0,0 +1,63 @@
+import { Image } from "primereact/image"
+import { Button } from "primereact/button"
+import { Dialog } from 'primereact/dialog'
+import GameImages from "../forms/GameImages"
+import  no_cover  from '../../../images/no-cover.jpg'
+import { useToggle } from "../../../hooks/useToggle"
+import { useState } from "react"
+
+const GameImageViewer = ({ game, handleImagesSubmit, style }) => {
+  const {toggle, toggleValue} = useToggle()
+  const [view, setView] = useState('cover')
+  const containerStyles = {
+    width: '100%',
+    ...style
+  }
+  return (
+    <div className="flex flex-column gap-2" style={containerStyles}>
+      <Dialog 
+          header="Game Images" 
+          visible={toggleValue} 
+          style={{ width: '50vw' }} 
+          onHide={toggle}
+        >
+          <GameImages onSubmit={handleImagesSubmit} />
+        </Dialog>
+      <section className="flex justify-content-center">
+        <h2>{game.name}</h2>
+      </section>
+      <section className="flex justify-content-center">
+        <Image 
+          src={game.cover || no_cover} 
+          alt={game.name} 
+          preview 
+          width={containerStyles.width} height="300" 
+        />
+      </section>
+      <section className="flex justify-content-center gap-3">
+        <Button
+          icon='pi pi-image'
+          className="p-button-outlined p-button-rounded p-button-warning edit-button"
+          onClick={() => setView('cover')}
+        />
+        <Button
+          icon='pi pi-mobile'
+          className="p-button-outlined p-button-rounded p-button-warning edit-button"
+          onClick={() => setView('cover-box')}
+        />
+        <Button
+          icon='pi pi-images'
+          className="p-button-outlined p-button-rounded p-button-warning edit-button"
+          onClick={() => setView('gallery')}
+        />
+        <Button
+          icon='pi pi-file-edit'
+          className="p-button-outlined p-button-rounded p-button-warning edit-button"
+          onClick={toggle}
+        />
+      </section>
+    </div>
+  )
+}
+
+export default GameImageViewer
