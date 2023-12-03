526e541a8c6beb887e477bd0d64f1819741eaeec
DRGWeiss <ruizd1710@gmail.com>
2023-12-03 10:22:56 -0500

    refactor: navigation through hook and dashboard navigation

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import UserCard from '../utils/cards/UserCard'
 import DashboardButton from "../utils/DashboardButton";
+import UserForm from "../utils/forms/UserForm";
 import { useToggle } from '../../hooks/useToggle';
 import { useUser } from '../../hooks/useUser';
 import { Dialog } from 'primereact/dialog';
-import UserForm from '../utils/forms/UserForm';
+import { useNavigation } from '../../hooks/useNavigation';
 
 const Dashboard = () => {
   const userFormToggle = useToggle()
   const { user, update } = useUser()
+  const navigator = useNavigation()
 
   return (
     <>
@@ -22,8 +24,8 @@ const Dashboard = () => {
       <section className="p-8">
         <UserCard user={user} update={update} />
         <div className="mt-6 flex flex-wrap gap-5 justify-content-center">
-          <DashboardButton icon="desktop" text="Games" />
-          <DashboardButton icon="bars" text="Stats" />
+          <DashboardButton icon="desktop" text="Games" action={navigator.goToPlayedGames} />
+          <DashboardButton icon="bars" text="Stats" action={navigator.goToStats} />
           <DashboardButton
             icon="search"
             text="Edit Info"
---
+++ src/hooks/useNavigation.js
@@ -0,0 +1,48 @@
+import { useNavigate } from "react-router-dom";
+import * as routes from "../routes";
+
+export const useNavigation = () => {
+  const navigator = useNavigate()
+
+  const navigateTo = (route) => {
+    navigator(route)
+  }
+
+  
+  const goToDashboard = () => {
+    navigateTo(routes.dashboard);
+  }
+  const goHome = () => {
+    navigateTo(routes.home);
+  }
+  const goToPlayedGames = () => {
+    navigateTo(routes.playedgames);
+  }
+  const goToUploadGame = () => {
+    navigateTo(routes.uploadgame);
+  }
+  const goToGameDetails = () => {
+    navigateTo(routes.gamedetails);
+  }
+  const goToStats = () => {
+    navigateTo(routes.stats);
+  }
+  const goToLogin = () => {
+    navigateTo(routes.login);
+  }
+  const goToRegister = () => {
+    navigateTo(routes.register);
+  }
+
+  return {
+    navigateTo,
+    goToDashboard,
+    goHome,
+    goToPlayedGames,
+    goToUploadGame,
+    goToGameDetails,
+    goToStats,
+    goToLogin,
+    goToRegister
+  }
+}
